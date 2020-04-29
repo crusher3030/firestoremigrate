1063a62778323fb4cb6d969f1ce75a47cde7401e
crusher3030 <52600314+crusher3030@users.noreply.github.com>
2020-04-28 19:36:16 -0500

    Delete credentials.js

--- credentials.js
+++
@@ -1,8 +0,0 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("path/to/serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://cayagolf-d6a1f.firebaseio.com"
-});
